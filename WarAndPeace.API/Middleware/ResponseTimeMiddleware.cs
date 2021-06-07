eee64bccc2b53e30cc0757b7ddb66f999ab381f2
Nikhil Ramdass (ZA) <nikhil.ramdass@absa.africa>
2021-06-07 19:30:53 +0200

    Fixed folders. ms to ticks

--- WarAndPeace.API/Middleware/ResponseTimeMiddleware.cs
+++ WarAndPeace.API/Middleware/ResponseTimeMiddleware.cs
@@ -8,7 +8,7 @@ namespace WarAndPeace.API.Middleware
 
     public class ResponseTimeMiddleware
     {
-        private const string RESPONSE_HEADER_RESPONSE_TIME = "X-Response-Time-ms";
+        private const string RESPONSE_HEADER_RESPONSE_TIME = "X-Response-Time-ticks";
         private readonly RequestDelegate _next;
 
         public ResponseTimeMiddleware(RequestDelegate next)
@@ -22,9 +22,8 @@ namespace WarAndPeace.API.Middleware
             watch.Start();
             context.Response.OnStarting(() =>
             {
-
                 watch.Stop();
-                var responseTimeForCompleteRequest = watch.ElapsedMilliseconds;
+                var responseTimeForCompleteRequest = watch.ElapsedTicks;
                 context.Response.Headers[RESPONSE_HEADER_RESPONSE_TIME] = responseTimeForCompleteRequest.ToString();
                 return Task.CompletedTask;
             });
